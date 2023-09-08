44559441f1eb6977a9ab3453412b7e08b2f399d7
diasdecarvalholeonardo <diasdecarvalholeonardo@gmail.com>
2023-09-08 00:47:19 -0300

    1º commit

---
+++ OnlineStore/src/test/java/br/com/ecommerce/onlinestore/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
